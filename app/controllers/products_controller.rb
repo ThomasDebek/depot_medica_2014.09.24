173288618df33504d89d0f7403e84f0faa99d477
Tomasz Dębek <tdebek@Mac-mini-Tomasz.local>
2014-09-26 11:15:08 +0200

    GEM PAPERCLIP do wyboru obrazka. bundle, rails g migration AddAvatarColumnsToProducts avatar, zmieniamy w nim metody wdg.dokumentacji, rake db:migrate, w kontrolerze zamiast new, na create oraz dodajemy w paramsach :avatar, modyfikujemy w products/ _form, index, show oraz na stronie glownej index :)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+    @product = Product.create(product_params)
 
     respond_to do |format|
       if @product.save
@@ -79,6 +79,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
+      params.require(:product).permit(:title, :description, :image_url, :price, :avatar)
     end
 end
