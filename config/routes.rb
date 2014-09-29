a1f939f28692ec4743cb32eded0cfe4d6c8f9e06
Tomasz Dębek <tdebek@Mac-mini-Tomasz.local>
2014-09-29 11:37:19 +0200

    Wyswietlanie danych jako json, dodajmey ciezke w routsach, w kontrolerze dodajmey odpowiednie katalogi i tworzymy moduly i metody, a w przegladarce klikamy : http://localhost:3000/api/v1/products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :products
+    end
+  end
+
   get 'admin' => 'admin#index'
   controller :sessions do
     get  'login' => :new
